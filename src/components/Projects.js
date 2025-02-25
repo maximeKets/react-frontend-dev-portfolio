e23c5168778727c484150ce1ce4f6bb9864614fe
MaximeInMind <maxime@inmind.fr>
2025-02-25 18:41:48 +0100

    upadte skills and inspirations

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,4 +1,8 @@
 import React, { Component } from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 import ProjectDetailsModal from "./ProjectDetailsModal";
 
 class Projects extends Component {
@@ -11,58 +15,92 @@ class Projects extends Component {
   }
 
   render() {
+    // Gère l'ouverture/fermeture du modal
     let detailsModalShow = (data) => {
       this.setState({ detailsModalShow: true, deps: data });
     };
-
     let detailsModalClose = () => this.setState({ detailsModalShow: false });
+
+    // Configuration du slider
+    const settings = {
+      dots: true,
+      infinite: false,
+      speed: 500,
+      slidesToShow: 3, // 3 slides en desktop
+      slidesToScroll: 1,
+      responsive: [
+        {
+          breakpoint: 768,     // Sous 768px, on passe à 1 slide
+          settings: {
+            slidesToShow: 1,
+          },
+        },
+      ],
+    };
+
+    let sectionName, projects;
+
     if (this.props.resumeProjects && this.props.resumeBasicInfo) {
-      var sectionName = this.props.resumeBasicInfo.section_name.projects;
-      var projects = this.props.resumeProjects.map(function (projects) {
+      sectionName = this.props.resumeBasicInfo.section_name.projects;
+
+      // On recrée vos "miniatures" mais on les met chacune dans un <div> pour le carousel
+      projects = this.props.resumeProjects.map((project, index) => {
         return (
-          <div
-            className="col-sm-12 col-md-6 col-lg-4"
-            key={projects.title}
-            style={{ cursor: "pointer" }}
-          >
-            <span className="portfolio-item d-block">
-              <div className="foto" onClick={() => detailsModalShow(projects)}>
-                <div>
-                  <img
-                    src={projects.images[0]}
-                    alt="projectImages"
-                    height="230"
-                    style={{marginBottom: 0, paddingBottom: 0, position: 'relative'}}
-                  />
-                  <span className="project-date">{projects.startDate}</span>
-                  <br />
-                  <p className="project-title-settings mt-3">
-                    {projects.title}
-                  </p>
+            <div key={index}className="d-flex justify-content-center">
+
+              <div
+                  className="col-sm-12 col-md-6 col-lg-8"
+                  style={{ cursor: "pointer" }}
+              >
+              <span className="portfolio-item d-block">
+                <div className="foto" onClick={() => detailsModalShow(project)}>
+                  <div>
+                    <img
+                        src={project.images[0]}
+                        alt="projectImages"
+                        height="230"
+                        style={{
+                          marginBottom: 0,
+                          paddingBottom: 0,
+                          position: "relative",
+                        }}
+                    />
+                    <span className="project-date">{project.startDate}</span>
+                    <br />
+                    <p className="project-title-settings mt-3">
+                      {project.title}
+                    </p>
+                  </div>
                 </div>
+              </span>
               </div>
-            </span>
-          </div>
+            </div>
         );
       });
     }
 
     return (
-      <section id="portfolio">
-        <div className="col-md-12">
-          <h1 className="section-title" style={{ color: "black" }}>
-            <span>{sectionName}</span>
-          </h1>
-          <div className="col-md-12 mx-auto">
-            <div className="row mx-auto">{projects}</div>
+        <section id="portfolio">
+          <div className="col-md-12">
+            <h1 className="section-title" style={{ color: "black" }}>
+              <span>{sectionName}</span>
+            </h1>
+
+            {/* On remplace la row bootstrap par le slider */}
+            <div className="col-md-12 mx-auto">
+              <Slider {...settings}>
+                {projects}
+              </Slider>
+            </div>
+
+            {/* Modal pour les détails du projet */}
+            <ProjectDetailsModal
+                show={this.state.detailsModalShow}
+                onHide={detailsModalClose}
+                data={this.state.deps}
+            />
           </div>
-          <ProjectDetailsModal
-            show={this.state.detailsModalShow}
-            onHide={detailsModalClose}
-            data={this.state.deps}
-          />
-        </div>
-      </section>
+        </section>
     );
   }
 }
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,44 +1,80 @@
 import React, { Component } from "react";
 
 class Skills extends Component {
-  render() {
-    if (this.props.sharedSkills && this.props.resumeBasicInfo) {
-      var sectionName = this.props.resumeBasicInfo.section_name.skills;
-      var skills = this.props.sharedSkills.icons.map(function (skills, i) {
-        return (
-          <li className="list-inline-item mx-3" key={i}>
-            <span>
-              <div className="text-center skills-tile">
-                <i className={skills.class} style={{ fontSize: "220%" }}>
-                  <p
-                    className="text-center"
-                    style={{ fontSize: "30%", marginTop: "4px" }}
-                  >
-                    {skills.name}
-                  </p>
-                </i>
-              </div>
-            </span>
-          </li>
-        );
-      });
-    }
+    render() {
+        let sectionName, skills, cards;
 
-    return (
-      <section id="skills">
-        <div className="col-md-12">
-          <div className="col-md-12">
-            <h1 className="section-title">
-              <span className="text-white">{sectionName}</span>
-            </h1>
-          </div>
-          <div className="col-md-12 text-center">
-            <ul className="list-inline mx-auto skill-icon">{skills}</ul>
-          </div>
-        </div>
-      </section>
-    );
-  }
+        // Vérifier la disponibilité des données
+        if (this.props.sharedSkills && this.props.resumeBasicInfo) {
+            // Récupération du nom de section
+            sectionName = this.props.resumeBasicInfo.section_name.skills;
+
+            // 1) Mapper les "icons" (compétences)
+            skills = this.props.sharedSkills.icons.map((skill, i) => {
+                return (
+                    <li className="list-inline-item mx-3" key={i}>
+                        <div className="text-center skills-tile">
+              <span className="material-icons" style={{ fontSize: "220%" }}>
+                {skill.iconName}
+              </span>
+
+                            <p
+                                className="text-center"
+                                style={{ fontSize: "60%", marginTop: "4px" }}
+                            >
+                                {skill.name}
+                            </p>
+                        </div>
+                    </li>
+                );
+            });
+
+            // 2) Mapper les "cards" (si elles existent)
+            if (this.props.sharedSkills.cards) {
+                cards = this.props.sharedSkills.cards.map((card, index) => {
+                    return (
+                        <div className="col-md-4 mb-4" key={index}>
+                            <div className="card" style={{ width: "18rem" }}>
+                                <img
+                                    className="card-img-top"
+                                    src={card.img}
+                                    alt="Card illustration"
+                                />
+                                <div className="card-body">
+                                    <h5 className="card-title">{card.title}</h5>
+                                    <p className="card-text">{card.text}</p>
+                                </div>
+                            </div>
+                        </div>
+                    );
+                });
+            }
+        }
+
+        return (
+            <section id="skills">
+                <div className="col-md-12">
+                    <div className="col-md-12">
+                        <h1 className="section-title">
+                            <span className="text-white">{sectionName}</span>
+                        </h1>
+                    </div>
+
+
+                    <div className="col-md-12 text-center">
+                        <ul className="list-inline mx-auto skill-icon">{skills}</ul>
+                    </div>
+
+
+                    {cards && (
+                        <div className="row justify-content-center mt-4">
+                            {cards}
+                        </div>
+                    )}
+                </div>
+            </section>
+        );
+    }
 }
 
 export default Skills;
